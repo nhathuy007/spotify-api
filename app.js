cf90f0fcb1991a64f29e2254c69d2ac92ff0634b
nhathuy007 <55850287+nhathuy007@users.noreply.github.com>
2019-11-14 13:15:29 -0800

    Update app.js

--- app.js
+++ app.js
@@ -25,10 +25,17 @@ var app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/client'));
+app.use(cors('https://spotify-webapi.herokuapp.com'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/', indexRouter);
 
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/client/index.html'));
 });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
